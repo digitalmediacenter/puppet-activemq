a15ce2a16eb344eadceca5e2cbad3a01fbf16c0e
Matthias Baur <matthias.baur@dmc.de>
2014-07-30 18:56:29 +0200

    Removed name parameter since its the same as the title

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,6 @@ class activemq::service (
 
   service { 'activemq':
     ensure     => running,
-    name       => 'activemq',
     hasrestart => true,
     hasstatus  => false,
     enable     => true,
